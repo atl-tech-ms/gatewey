cf8dd83a1584841900479e7ab821af909bbd4241
thehasanovv <hasanov.e95@gmail.com>
2024-09-17 12:46:54 +0400

    gateway routerlocator

--- src/main/java/az/atl/gatewayms/route/RouteLocatorConfig.java
+++ src/main/java/az/atl/gatewayms/route/RouteLocatorConfig.java
@@ -1,21 +1,34 @@
 package az.atl.gatewayms.route;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class RouteLocatorConfig {
+
+    @Value("${app.url.customer}")
+    private String customer;
+
+    @Value("${app.url.order}")
+    private String order;
+
+    @Value("${app.url.product}")
+    private String product;
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("path", r -> r.path("/customer-ms/**")
-                        .uri("http://localhost:8080"))
+                        .uri(customer))
                 .route("path", r -> r.path("/api/v1/orders/**")
-                        .uri("http://localhost:8081"))
+                        .uri(order))
                 .route("path", r -> r.path("/api/v1/products/**")
-                        .uri("http://localhost:8082"))
+                        .uri(product))
                 .build();
     }
 }
